7887a9695d6939fb902363696e0225254a857831
Santoso Budi <b_prast@hotmail.com>
2020-05-13 17:39:20 +0900

    Add func viewDidLayoutSub

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         //
     }
-
+    override func viewDidLayoutSubviews() {
+        //
+    }
 }
-
